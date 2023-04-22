0327a5c23355445c4200e000f105d802e3a716ce
Sibu07 <91250474+Sibu07@users.noreply.github.com>
2023-04-22 16:08:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,36 @@
 import json
-
 import requests
-
 from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
-
 def get_data():
-
     json_url = request.args.get('json_url')
-
     start = request.args.get('start')
-
     end = request.args.get('end')
-
     limit = request.args.get('limit')
 
     response = requests.get(json_url)
-
     json_data = json.loads(response.text)
 
     if start:
-
         start = int(start)
-
     else:
-
         start = 0
 
     if end:
-
         end = int(end)
-
     else:
-
         end = len(json_data)
 
     if limit:
-
         limit = int(limit)
-
         end = start + limit
 
     response_data = json_data[start:end]
-
     response = Response(json.dumps(response_data), mimetype='application/json')
-
     return response
 
 if __name__ == '__main__':
-
-    app.run()
-
+    app.run(port=8000)
